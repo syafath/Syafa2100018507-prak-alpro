1a40b4685bf644212cb59b648e43d2b3836c363b
SYAFA <trisya.syafa@gmail.com>
2022-03-19 23:10:01 +0700

    membuat Calculator-cpp

---
+++ PRA PRAKTIKUM/Calculator.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+int main(){
+    a:
+    cout << "=========================\n";
+    cout << "\tKalkulator\n";
+    cout << "=========================\n";
+    
+    int plh, coba;
+    float x, y;
+    cout << "1. penjumlahan\n";
+    cout << "2. pembagian\n";
+    cout << "3. perkalian\n";
+    cout << "4. pengurangan\n";
+    cout << "Pilihan = ";
+	cin >> plh;
+    cout << endl;
+
+    if (plh <= 4 && plh >= 1){
+        cout << "Angka pertama = ";
+		cin >> x;
+        cout << "Angka kedua = ";
+		cin >> y;
+        switch (plh){
+	        case 1:
+	            cout << " " << x << " + " << y << " = " << x + y << "\n";
+				break;
+	        case 2:
+	            cout << " " << x << " : " << y << " = " << x / y << "\n";
+				break;
+	        case 3:
+	            cout << " " << x << " x " << y << " = " << x * y << "\n";
+				break;
+	        case 4:
+	            cout << " " << x << " - " << y << " = " << x - y << "\n";
+				break;
+        }
+    }
+    else{
+        cout << "SALAH!!!" << "\n";
+    }
+        cout << endl;
+        cout << "Apakah ingin mencoba lagi [1] Ya / [2] Tidak => ";
+        cin >> coba;
+        if (coba == 1){
+            system ("cls");
+            goto a;
+        }
+        else if(coba == 2){
+            system ("cls");
+        }
+        
+    return 0;
+}
